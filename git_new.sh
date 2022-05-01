ae873895841d5481fb60cb50cf205cdcd94ca789
littlebee21 <805976520@qq.com>
2022-05-01 19:09:32 +0800

    all commit

---
+++ git_new.sh
@@ -0,0 +1,25 @@
+# !bin/bash/
+
+git init
+
+#git clone git@github.com:littlebee21/HuaQing.git
+
+#add file to push
+git checkout -b
+
+#set branch and respos
+git branch -M main
+
+# 设置远程仓库，ssh方式的不需要使用密码
+# git remote add origin https://github.com/littlebee21/siwei.git
+git remote add origin git@github.com:littlebee21/picmind.git
+
+
+git add .
+git commit -m "all commit"
+
+
+
+#orgin 是起源的意思 main是分治
+git push -u origin main
+#git push -u -f orgin main
